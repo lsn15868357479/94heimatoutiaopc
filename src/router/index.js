443a84604868215b477091098588578152919bd5
lsn15868357479 <2658523464@qq.com>
2020-03-01 15:49:14 +0800

    页面分析-新建登录和主页

--- src/router/index.js
+++ src/router/index.js
@@ -2,22 +2,32 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 // import Home from '../views/Home.vue'
 import Home from '@/views/home'// 用一种简写 的形式替换原有的方式
+import Login from '@/views/login'
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home', // 强制跳转到home页面
+    redirect: '/home'// 强制跳转到home页面
+  },
+  {
+    path: '/home',
+    name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    component: Login,
+    name: 'Login'
   }
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
---
+++ src/views/login/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>我是登录页面</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
