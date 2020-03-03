ab9c6e6c08cbfa29f09b1ba2ea9f74e3404e062f
lsn15868357479 <2658523464@qq.com>
2020-03-03 14:40:08 +0800

    主页模块-头部结构及下拉菜单

---
+++ src/components/home/layout-header.vue
@@ -0,0 +1,56 @@
+<template>
+<!-- elementUI布局组件 el-row 和 el-col  -->
+<el-row align="middle" type='flex' class='layout-header'>
+<!-- 两列 -->
+ <el-col class='left' :span="12">
+    <!-- 图标 -->
+     <i class='el-icon-s-fold'></i>
+    <span> 江苏传智播客教育科技股份有限公司 </span>
+</el-col>
+<!-- 右侧列 -->
+    <el-col class='right' :span="12">
+        <!-- 再次放置一个row组件-->
+        <el-row type='flex' justify="end" align="middle">
+          <img src="../../assets/img/avatar.jpg" alt="">
+           <!-- 下拉菜单 -->
+           <el-dropdown trigger='click'>
+               <!-- 显示的内容 -->
+               <span>大魔王</span>
+                <!-- 下拉内容需要做具名插槽dropdown  el-dropdown-menu是专门做下拉的组件 -->
+                <el-dropdown-menu slot="dropdown" >
+                   <!-- 下拉选项 el-dropdown-item 作为下拉选项组件-->
+                    <el-dropdown-item>个人信息</el-dropdown-item>
+                    <el-dropdown-item>git地址</el-dropdown-item>
+                    <el-dropdown-item>退出</el-dropdown-item>
+
+                </el-dropdown-menu>
+           </el-dropdown>
+        </el-row>
+    </el-col>
+</el-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less' scoped>
+.layout-header {
+    height: 60px;
+    .left {
+        i{
+            font-size: 20px;
+        }
+    }
+    .right {
+        img {
+            width: 40px;
+            height: 40px;
+            border-radius: 50%;
+            margin-right: 4px;
+        }
+    }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,23 +1,37 @@
 <template>
-<!-- 先定义一个大容器 -->
-<el-container>
-  <!-- 先放置一个左侧 -->
-  <el-aside>左侧内容</el-aside>
-  <!-- 右侧大容器 -->
-  <el-container>
-    <!-- 头部 -->
-    <el-header>头部</el-header>
-    <!-- 中间区域 -->
-    <el-main>
-      <!-- 二级路由容器 -->
-      <router-view></router-view>
-    </el-main>
-  </el-container>
-</el-container>
+<!-- 先在最外层放置一个大容器 因为 小容器只能放到大容器里面 -->
+   <el-container>
+     <el-aside style="width:230px">
+       <!-- 左侧导航组件 -->
+       <layout-aside></layout-aside>
+     </el-aside>
+     <!-- 右侧 再嵌套一个 el-container -->
+     <el-container>
+       <!-- 再上下布局 -->
+       <el-header>
+         <!-- 顶部操作栏组件 -->
+         <layout-header></layout-header>
+       </el-header>
+       <!-- 主要区域 -->
+       <el-main>
+         <h1>主要区域</h1>
+          <!-- 这里应该放置 二级路由容器 -->
+         <router-view></router-view>
+       </el-main>
+     </el-container>
+   </el-container>
 </template>
 
 <script>
-export default {}
+// 将左侧导航组件引用 注册 并使用
+import LayoutAside from '@/components/home/layout-aside'
+import LayoutHeader from '@/components/home/layout-header'
+export default {
+  components: {
+    'layout-aside': LayoutAside,
+    'layout-header': LayoutHeader
+  }
+}
 </script>
 
 <style>
