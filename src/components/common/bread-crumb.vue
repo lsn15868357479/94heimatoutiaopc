3c0bd0cdaed8767eef567e18440df24f48c3bdda
lsn15868357479 <2658523464@qq.com>
2020-03-04 11:39:41 +0800

    黑马头条PC-面包屑组件封装

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,21 @@
+<template>
+<el-breadcrumb>
+    <el-breadcrumb-item to="/home">首页</el-breadcrumb-item>
+    <!-- 第二是动态的选项 -->
+    <el-breadcrumb-item>
+        <!-- 内容是动态的 -->
+        <!-- 定义一个具名插槽 -->
+        <slot name="title"></slot>
+    </el-breadcrumb-item>
+</el-breadcrumb>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
