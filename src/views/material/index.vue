650896e7d3d09d5a08956ce2993bdabc1d9d69b0
lsn15868357479 <2658523464@qq.com>
2020-03-07 14:35:57 +0800

    素材列表-数据加载

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -10,16 +10,32 @@
         </bread-crumb>
         <!-- {{activeName}} -->
         <!-- 放置标签页 -->
-          <!-- 放置标签页 v-model所绑定的值 就是当前所激活的页签-->
-        <el-tabs v-model="activeName">
+          <!-- 放置标签页 v-model所绑定的值 就是当前所激活的页签 切换tabs页签的时候 需要 进行事件的监听-->
+        <el-tabs v-model="activeName" @tab-click="changeTab">
               <!-- 放置标签 label表示标签显示的名称 name代表页签的选中的值-->
             <el-tab-pane label="全部素材" name="all">
-                全部素材
-                </el-tab-pane>
+               <div class="img-list">
+                 <el-card class="img-card" v-for="item in list" :key="item.id">
+                   <!-- 放置图片 并且赋值图片地址-->
+                   <img :src="item.url" alt="">
+                   <!-- 操作栏 -->
+                   <el-row class='operate' type='flex' align="middle" justify="space-around">
+                           <i class='el-icon-star-on'></i>
+                           <i class='el-icon-delete-solid'></i>
+                   </el-row>
+                 </el-card>
+               </div>
+            </el-tab-pane>
 
             <el-tab-pane label="收藏素材" name="collect">
-                收藏素材
-                </el-tab-pane>
+                <div class='img-list'>
+                    <!-- 采用v-for对list数据进行循环 -->
+                    <el-card class='img-card' v-for="item in list" :key="item.id">
+                        <!-- 放置图片 并且赋值 图片地址-->
+                        <img :src="item.url" alt="">
+                    </el-card>
+                </div>
+            </el-tab-pane>
         </el-tabs>
    </el-card>
 </template>
@@ -28,11 +44,62 @@
 export default {
   data () {
     return {
-      activeName: 'all' // 当前激活的标签
+      activeName: 'all', // 当前激活的标签
+      list: []// 接收全部素材
     }
+  },
+  methods: {
+    // 获取素材数据
+    getMaterial () {
+      this.$axios({
+        url: '/user/images', // 请求地址
+        params: {
+          collect: this.activeName === 'collect' //  这个位置应该变活 根据当前的页签变活   activeName === 'all' 获取所有的素材  activeName = 'collect' 获取收藏素材数据
+        }, // het参数就是query参数
+        data: {} // data参数 放的是bodycs
+      }).then(result => {
+        // 讲返回的数据 赋值到data中的数据
+        this.list = result.data.results
+      })
+    },
+    // 切换页签事件
+    changeTab () {
+      // 在切换事件中
+    // 可以根据当前 activeName来决定是获取哪个方面 的数据
+    // activeName === 'all' 获取所有的素材  activeName = 'collect' 获取收藏素材
+      this.getMaterial() // 直接调用获取素材的方法
+    }
+  },
+  created () {
+    // 实例化之后 调用获取素材数据
   }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.img-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  .img-card {
+    width: 220px;
+    height: 240px;
+    margin: 20px 30px;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+    .operate {
+       position: absolute;
+       left:0;
+       bottom:0;
+       width: 100%;
+       background: #f4f5f6;
+       height: 30px;
+       i  {
+            font-size:20px;
+         }
+    }
+  }
+}
 </style>
