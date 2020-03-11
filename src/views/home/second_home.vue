0fce9dd527fae0b8d7c11ef24de639ba7ae57b34
lsn15868357479 <2658523464@qq.com>
2020-03-11 16:56:53 +0800

    黑马头条PC-echarts在项目中的应用

--- src/views/home/second_home.vue
+++ src/views/home/second_home.vue
@@ -7,11 +7,197 @@
          <img :src="item" alt="">
       </el-carousel-item>
     </el-carousel>
+    <!-- 放置图表组件 -->
+   <el-row type='flex' justify="space-around">
+      <div ref="myChart" class="echarts"></div>
+       <div ref="myChart2" class="echarts"></div>
+   </el-row>
 </div>
 </template>
 
 <script>
+// 引入图表模块
+// 图表本身是用canvas实现的
+import Echarts from 'echarts'
 export default {
+  mounted () {
+    // 进行echart的图表实例化
+    this.myChart = Echarts.init(this.$refs.myChart)
+    this.myChart2 = Echarts.init(this.$refs.myChart2)
+    // this.myChart 就是图表对象
+    this.myChart.setOption({
+      title: {
+        text: '堆叠区域图'
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'cross',
+          label: {
+            backgroundColor: '#6a7985'
+          }
+        }
+      },
+      legend: {
+        data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {}
+        }
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: 'category',
+          boundaryGap: false,
+          data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value'
+        }
+      ],
+      series: [
+        {
+          name: '邮件营销',
+          type: 'line',
+          stack: '总量',
+          areaStyle: {},
+          data: [120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+          name: '联盟广告',
+          type: 'line',
+          stack: '总量',
+          areaStyle: {},
+          data: [220, 182, 191, 234, 290, 330, 310]
+        },
+        {
+          name: '视频广告',
+          type: 'line',
+          stack: '总量',
+          areaStyle: {},
+          data: [150, 232, 201, 154, 190, 330, 410]
+        },
+        {
+          name: '直接访问',
+          type: 'line',
+          stack: '总量',
+          areaStyle: {},
+          data: [320, 332, 301, 334, 390, 330, 320]
+        },
+        {
+          name: '搜索引擎',
+          type: 'line',
+          stack: '总量',
+          label: {
+            normal: {
+              show: true,
+              position: 'top'
+            }
+          },
+          areaStyle: {},
+          data: [820, 932, 901, 934, 1290, 1330, 1320]
+        }
+      ]
+    })
+
+    this.myChart2.setOption({
+      tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b}: {c} ({d}%)'
+      },
+      legend: {
+        orient: 'vertical',
+        left: 10,
+        data: ['直达', '营销广告', '搜索引擎', '邮件营销', '联盟广告', '视频广告', '百度', '谷歌', '必应', '其他']
+      },
+      series: [
+        {
+          name: '访问来源',
+          type: 'pie',
+          selectedMode: 'single',
+          radius: [0, '30%'],
+
+          label: {
+            position: 'inner'
+          },
+          labelLine: {
+            show: false
+          },
+          data: [
+            { value: 335, name: '直达', selected: true },
+            { value: 679, name: '营销广告' },
+            { value: 1548, name: '搜索引擎' }
+          ]
+        },
+        {
+          name: '访问来源',
+          type: 'pie',
+          radius: ['40%', '55%'],
+          label: {
+            formatter: '{a|{a}}{abg|}\n{hr|}\n  {b|{b}：}{c}  {per|{d}%}  ',
+            backgroundColor: '#eee',
+            borderColor: '#aaa',
+            borderWidth: 1,
+            borderRadius: 4,
+            // shadowBlur:3,
+            // shadowOffsetX: 2,
+            // shadowOffsetY: 2,
+            // shadowColor: '#999',
+            // padding: [0, 7],
+            rich: {
+              a: {
+                color: '#999',
+                lineHeight: 22,
+                align: 'center'
+              },
+              // abg: {
+              //     backgroundColor: '#333',
+              //     width: '100%',
+              //     align: 'right',
+              //     height: 22,
+              //     borderRadius: [4, 4, 0, 0]
+              // },
+              hr: {
+                borderColor: '#aaa',
+                width: '100%',
+                borderWidth: 0.5,
+                height: 0
+              },
+              b: {
+                fontSize: 16,
+                lineHeight: 33
+              },
+              per: {
+                color: '#eee',
+                backgroundColor: '#334455',
+                padding: [2, 4],
+                borderRadius: 2
+              }
+            }
+          },
+          data: [
+            { value: 335, name: '直达' },
+            { value: 310, name: '邮件营销' },
+            { value: 234, name: '联盟广告' },
+            { value: 135, name: '视频广告' },
+            { value: 1048, name: '百度' },
+            { value: 251, name: '谷歌' },
+            { value: 147, name: '必应' },
+            { value: 102, name: '其他' }
+          ]
+        }
+      ]
+    })
+  },
   data () {
     return {
       list: [
@@ -34,6 +220,11 @@ export default {
   img {
     width: 100%;
     height: 100%;
+  }
+  .echarts{
+    width: 600px;
+    height: 350px;
+    background-color:lightpink;
   }
 }
 </style>
